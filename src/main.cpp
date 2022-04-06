b0c42e859b0350056573164573b0093100011a83
Stefan <stefanvr@gmail.com>
2022-04-06 23:49:33 +0200

    measuring long range

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,48 @@
-
 #include <Arduino.h>
+#include "Adafruit_VL53L0X.h" 
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
 
-#define LED 2
 
-void setup() {
-  // put your setup code here, to run once:
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup(){
   Serial.begin(115200);
-  pinMode(LED, OUTPUT);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial)
+  {
+  delay(1);
+  }
+
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin(VL53L0X_I2C_ADDR, false, &Wire, Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE))
+  {
+  Serial.println(F("Failed to boot VL53L0X"));
+  while(1);
+  }
+  // power
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  Serial.println("LED is on");
-  delay(100);
-  digitalWrite(LED, LOW);
-  Serial.println("LED is off");
+void loop(){
+  // set cursor to first column, first row
+  VL53L0X_RangingMeasurementData_t measure;
+
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4)
+  { // phase failures have incorrect data
+  Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  }
+  else
+  {
+  Serial.println(" out of range ");
+  }
+
+  
   delay(100);
 }
