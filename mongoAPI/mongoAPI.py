c06de40290d4cc51f8ea6ed34e186d61430b6e04
Stefan <stefanvr@gmail.com>
2022-04-08 23:55:15 +0200

    draft version of mongoapi added

---
+++ mongoAPI/mongoAPI.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request
+from pymongo import MongoClient
+from bson.json_util import dumps
+import json
+
+import _secrets 
+
+client = MongoClient(_secrets.mongo_url)
+database = 'mongoAPI'
+collection = 'test'
+
+app = Flask(__name__)
+
+@app.route("/add", methods = ['POST'])
+def add():
+    try:
+        print(request.data)
+        data = json.loads(request.data)
+        print(data)
+        client[database][collection].insert_one(data)
+        return dumps({'message' : 'SUCCESS'})
+    except Exception as e:
+        print(e)
+        return dumps({'error' : str(e)})
+
+if __name__ == '__main__':
+  
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(host='0.0.0.0', port=5001, debug=True)
