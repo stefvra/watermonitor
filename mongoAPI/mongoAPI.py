d5e9f2c390ed3a89bda1d843f89ea184c6873420
Stefan <stefanvr@gmail.com>
2022-04-15 11:48:23 +0200

    serverport moved to separate file

--- mongoAPI/mongoAPI.py
+++ mongoAPI/mongoAPI.py
@@ -30,4 +30,4 @@ if __name__ == '__main__':
   
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=_secrets.serverport, debug=True)
